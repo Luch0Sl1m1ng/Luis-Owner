463eb1a2428b19aec074cd96a23fb16fd52c4e34
Luis Rojas Gil <lu.rojasg@duocuc.cl>
2024-06-28 21:46:36 -0400

    Archivos csv
    Esto sirve para crear y leer archivos csv en python

---
+++ archivos_csv.py.py
@@ -0,0 +1,34 @@
+"""
+import csv
+
+#Crear archivo csv
+with open('nuevo_archivo.csv', 'w', newline='', encoding='utf-8') as archivo_csv:
+    escritor_csv = csv.writer(archivo_csv)
+# Escribir una fila en el archivo CSV
+    escritor_csv.writerow(['Nombre', 'Edad', 'Comuna'])
+# Escribir múltiples filas en el archivo CSV
+    escritor_csv.writerows([
+['Esteban', 25, 'Santiago'],
+['María', 30, 'Valparaíso'],
+['Carlos', 22, 'Osorno'],
+['Sigrid', 25, 'Santiago'],
+['Daniela', 30, 'La Cisterna'],
+['Aylen', 22, 'La florida']
+])
+    
+print("¡Se ha creado el archivo exitosamente!")
+"""
+
+
+# Leer Archivos CSv
+import csv
+# Sintaxis: open('nombre_del_archivo.csv', 'modo', newline='')
+# Modo común: 'r' (lectura)
+with open('nuevo_archivo.csv', 'r', newline='', encoding='utf-8') as archivo_csv:
+    lector_csv = csv.reader(archivo_csv)
+    print("Las filas dentro del archivo csv son: ")
+    for fila in lector_csv:
+        print(fila)
+
+print("El lectro completo del csv es: ")
+print(lector_csv)
